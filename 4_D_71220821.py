9af692a5774ad39a73b91d314fbff1b1576da8bc
StefaniHrt <117893405+StefaniHrt@users.noreply.github.com>
2023-03-01 19:02:48 +0700

    Add files via upload

---
+++ 4_D_71220821.py
@@ -0,0 +1,9 @@
+bilangan=list(input('Masukkan sekumpulan bilangan (pisahkan dengan koma):').split(','))
+bil=[]
+for i in bilangan:
+    B=int(i)
+    bil.append(B)
+terbesar=lambda bil: max(bil)
+terkecil=lambda bil: min(bil)
+print('Bilangan terbesar dari kumpulan bilangan yang di input adalah', terbesar(bil))
+print('Bilangan terkecil dari kumpulan bilangan yang di input adalah', terkecil(bil))
